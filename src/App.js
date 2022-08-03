404e5197f0b65557596a553bd891bde36bc41e99
Arjya Paul <arjyapaul14@gmail.com>
2022-08-04 02:18:51 +0530

    Pages done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Navs from './components/navs';
+import Home from './pages/home';
+import Starred from './pages/starred';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return( 
+    <div>
+    <Navs/>
+    
+  <Switch>
+    <Route exact path="/"><Home/></Route>
+    <Route exact path="/starred"><Starred/></Route>
+    <Route>This is 404 page</Route>
+  </Switch>
+  </div>
   );
 }
 
---
+++ src/pages/starred.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Starred() {
+  return (
+    <div>
+      Welcome to the Starred Page
+    </div>
+  )
+}
+
+export default Starred
