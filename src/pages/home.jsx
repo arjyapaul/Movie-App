a3cf3e6f60e83b6118a005acf0b3d819a8c72065
Arjya Paul <arjyapaul14@gmail.com>
2022-08-04 21:11:35 +0530

    radiobuttons & actor searching

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,10 @@ import {apiGet} from '../misc/config'
 function Home() {
   const[input,setInput]=useState('');
   const[results,setResults]=useState(null);
+  const[searchOption,setSearchOption]=useState('shows');
+  const isShowsSearch=searchOption==='shows'
   const onSearch=()=>{
-    apiGet(`/search/shows?q=${input}`).then(result=>{
+    apiGet(`/search/${searchOption}?q=${input}`).then(result=>{
       setResults(result);
       console.log(result);
     });
@@ -19,18 +21,35 @@ function Home() {
       onSearch();
     }
   }
+  const onRadioChange=(ev)=>{
+    setSearchOption(ev.target.value);
+  }
   const renderResults=()=>{
     if(results && results.length===0){
       return <div>No Results</div>
     }
     if(results && results.length>0){
-      return <div>{results.map((item)=><div key={item.show.id}>{item.show.name}</div>)}</div>
+      return results[0].show ? results.map(item=>(<div key={item.show.id}>{item.show.name}</div>
+      )): results.map(item=>(<div key={item.person.id}>{item.person.name}</div>)); 
     }
     return null;
   }
   return (
     <MainPageLayout>
-      <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+      <input type="text" placeholder='search now'
+       onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
+       <div>
+        <label htmlFor='shows-search'>
+          shows
+          <input id="shows-search" type="radio" value="shows" checked={isShowsSearch} 
+          onChange={onRadioChange}/>
+        </label>
+        <label htmlFor='actors-search'>
+          actors
+          <input id="actors-search" type="radio" value="people" checked={!isShowsSearch} 
+          onChange={onRadioChange}/>
+        </label>
+       </div>
       <button type='button' onClick={onSearch}>Search</button>
       {renderResults()}
     </MainPageLayout>
